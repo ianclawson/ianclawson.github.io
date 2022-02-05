0861b5025b5f0008f2a67d0d40026662de6d9125
Ian Clawson <ianpaclawson@gmail.com>
2022-02-05 15:26:09 -0700

    make it work

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "IanClawsonDev",
+    platforms: [
+        .macOS("12.0")
+    ],
     products: [
         .executable(
             name: "IanClawsonDev",
