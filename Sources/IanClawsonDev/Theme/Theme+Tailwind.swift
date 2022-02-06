28de5dd0ebe16a60909451e2062d7b65d79ad785
Ian Clawson <ianpaclawson@gmail.com>
2022-02-06 12:00:29 -0700

    an optimistic rename

--- Sources/IanClawsonDev/Theme/Theme+Tailwind.swift
+++ Sources/IanClawsonDev/Theme/Theme+Tailwind.swift
@@ -1,6 +1,6 @@
 //
-//  MyHTMLFactory.swift
-//
+//  MyTheme.swift
+//  
 //
 //  Created by Ian Clawson on 2/5/22.
 //
@@ -8,6 +8,17 @@
 import Foundation
 import Publish
 import Plot
+
+/// theme feels like a misnomer to me as it's not a color/asset pallet but the entire HTML later
+/// if Publish dependency-injected your WebsiteItemMetadata here instead of having the the HTML
+/// functions fetch the data then I could see it, but I digress, publish is still the best
+extension Theme where Site == IanClawsonDev {
+    static var myTheme: Self {
+        Theme(
+            htmlFactory: MyHTMLFactory(),
+            resourcePaths: ["Resources/css/styles.css"])
+    }
+}
 
 struct MyHTMLFactory: HTMLFactory {
     func makeIndexHTML(for index: Index, context: PublishingContext<IanClawsonDev>) throws -> HTML {
