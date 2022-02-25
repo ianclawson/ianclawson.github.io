3792c0bc8cc90ac6270695a299a0dd4b5dd66c88
Ian Clawson <ianpaclawson@gmail.com>
2022-02-25 00:37:42 -0700

    margin

--- Sources/IanClawsonDev/Theme/Components/AppPageContentView.swift
+++ Sources/IanClawsonDev/Theme/Components/AppPageContentView.swift
@@ -17,7 +17,7 @@ struct AppPageContentView: Component {
 //        Div(item.content.body).class("content")
         Node<Any>.raw(
 """
-<div class="text-left prose sm:prose lg:prose-lg xl:prose-xl mx-auto app-page-content">
+<div class="text-left prose sm:prose lg:prose-lg xl:prose-xl mx-auto app-page-content mt-20 mb-20">
   \(item.content.body.html)
 </div>
 """
